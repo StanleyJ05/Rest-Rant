b9be926af124633b359036d939657566be4c1f9d
Stanley Jackson <stanleyjackson11.sj@gmail.com>
2023-04-02 21:27:13 -0700

    updated show page

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
+// Modules and Globals
 require('dotenv').config()
 const methodOverride = require('method-override')
 const express = require('express')
 const app = express()
 
-
-app.set('views',__dirname +'/views')
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
-
+// Controllers and Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-  res.render('home')
+    res.render('home')
 })
 
 app.get('*', (req, res) => {
-  res.status(404).render('error404')
+    res.render('error404')
 })
 
-
-app.listen(process.env.PORT)
-
+// Listen for Connections
+app.listen(process.env.PORT, () => { 
+    console.log("Listening on port ", process.env.PORT)
+})
