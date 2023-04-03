066a5d35b0eeb2fc4894ffc054cdd963422e1dad
Stanley Jackson <stanleyjackson11.sj@gmail.com>
2023-04-02 22:54:01 -0700

    process to deploy

--- controllers/places.js
+++ controllers/places.js
@@ -17,57 +17,31 @@ router.get('/:id', (req, res) => {
 })
 
 router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.redirect('/places')
-  }
+  db.place_schema.findByIdAndDelete(String(req.params.id))
+        .then(() => { res.redirect('/places') })
+        .catch((err) => {
+            console.log(err)
+            res.render('error404')
+        })
 })
 
 router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
+  db.place_schema.findById(req.params.id)
+  .then((place) => { res.render('places/edit', { place }) })
+  .catch((err) => {
+      console.log(err)
       res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id] })
-  }
+  })    
 })
 
 router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
+  db.place_schema.findByIdAndUpdate(String(req.params.id), req.body)
+  .then(() => { res.redirect(`/places/${req.params.id}`) })
+  .catch((err) => {
+      console.log(err)
       res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-      // Dig into req.body and make sure data is valid
-      if (!req.body.pic) {
-          // Default image if one is not provided
-          req.body.pic = 'http://placekitten.com/400/400'
-      }
-      if (!req.body.city) {
-          req.body.city = 'Anytown'
-      }
-      if (!req.body.state) {
-          req.body.state = 'USA'
-      }
+  })
 
-      // Save the new data into places[id]
-      places[id] = req.body
-      res.redirect(`/places/${id}`)
-  }
 })
 router.get('/', (req, res) => {
   db.Place.find()
